4a846df48fff6627f49bac9489d77e8d16780117
Sodiumlightbaby <edvard@xox.no>
2024-08-23 17:27:46 +0200

    Add Power swtich support for Rev 1.3 hardware

--- src/mia/sys/pwr.c
+++ src/mia/sys/pwr.c
@@ -34,11 +34,17 @@ void pwr_init(void)
         pwr_state = PWR_ERROR;
     }*/
 
+   //TODO: Power switch setup is currently not optimal while supporting both 1.2 and 1.3 devices
    //Turn off EXT power switch if powered from USB
    if(pwr_sense_vbus && !pwr_sense_vext){
         ext_set_dir(EXT_SWVEXT, true);
         ext_put(EXT_SWVEXT, false);
         pwr_state = PWR_VBUS;
+   //Turn on VBUS power switch if powered from EXT and not USB 
+   }else if(pwr_sense_vext && !pwr_sense_vbus){
+        ext_set_dir(EXT_SWVBUS, true);
+        ext_put(EXT_SWVBUS, true);
+        pwr_state = PWR_VEXT;
    }
 
 }
