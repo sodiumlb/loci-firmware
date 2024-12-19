51a369d9560fb84f88e8e72fd20c9ff13206b2a4
iss <iss@>
2024-12-19 22:38:04 +0200

    minor changes

--- src/mia/oric/acia.h
+++ src/mia/oric/acia.h
@@ -11,10 +11,17 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#ifndef ACIA_ORIC_EXOS_COMPAT
 #define ACIA_IO_DATA 0x0380
 #define ACIA_IO_STAT 0x0381
 #define ACIA_IO_CMD  0x0382
 #define ACIA_IO_CTRL 0x0383
+#else
+#define ACIA_IO_DATA 0x0340
+#define ACIA_IO_STAT 0x0341
+#define ACIA_IO_CMD  0x0342
+#define ACIA_IO_CTRL 0x0343
+#endif
 
 /* Kernel events
  */
@@ -22,6 +29,7 @@
 void acia_init(void);
 void acia_task(void);
 void acia_stop();
+void acia_run();
 
 void __not_in_flash() acia_reset(bool hw_reset);
 void __not_in_flash() acia_clr_irq(void);
